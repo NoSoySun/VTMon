4c9bfe81e32bc0e733c5615f466203fb1bfe691b
NoSoySun <sunga@DESKTOP-7VPA03N>
2026-05-11 10:42:59 -0500

    1.5.4 version's out :D

--- src/main/resources/data/cobblemon/abilities/battalion.js
+++ src/main/resources/data/cobblemon/abilities/battalion.js
@@ -1,29 +1,44 @@
 {
-    getSpeciesId(pokemon) {
-      return pokemon.species.id;
-    },
-    isBattalionForm(pokemon) {
-      const speciesId = this.getSpeciesId(pokemon);
-      return speciesId === "tokarmybattalion" || speciesId === "tokarmy_battalion" || speciesId === "tokarmy-battalion";
+    onStart(pokemon) {
+        if (pokemon.baseSpecies.baseSpecies !== 'Tokarmy' || pokemon.level < 20 || pokemon.transformed) return;
+
+        if (pokemon.hp > pokemon.maxhp / 4) {
+            if (pokemon.species.id === 'tokarmy') {
+                pokemon.formeChange('Tokarmy-Battalion');
+            }
+        } else {
+            if (pokemon.species.id === 'tokarmy-battalion' || 
+                pokemon.species.id === 'tokarmybattalion' || 
+                pokemon.species.id === 'tokarmy_battalion') {
+                pokemon.formeChange('Tokarmy');
+            }
+        }
     },
 
-    onStart(pokemon) {
-      if (pokemon.baseSpecies.baseSpecies !== "Tokarmy" || pokemon.level < 20 || pokemon.transformed)
-        return;
-      const speciesId = this.getSpeciesId(pokemon);
-      if (pokemon.hp > pokemon.maxhp / 4) {
-        if (speciesId === "tokarmy") {
-          pokemon.formeChange("Tokarmy-Battalion");
+    onResidualOrder: 29,
+    onResidual(pokemon) {
+        if (
+            pokemon.baseSpecies.baseSpecies !== 'Tokarmy' || 
+            pokemon.level < 20 ||
+            pokemon.transformed || 
+            !pokemon.hp
+        ) return;
+
+        if (pokemon.hp > pokemon.maxhp / 4) {
+            if (pokemon.species.id === 'tokarmy') {
+                pokemon.formeChange('Tokarmy-Battalion');
+            }
+        } else {
+            if (pokemon.species.id === 'tokarmy-battalion' || 
+                pokemon.species.id === 'tokarmybattalion' || 
+                pokemon.species.id === 'tokarmy_battalion') {
+                pokemon.formeChange('Tokarmy');
+            }
         }
-      } else {
-        if (this.isBattalionForm(pokemon)) {
-          pokemon.formeChange("Tokarmy");
-        }
-      }
     },
     
     flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, cantsuppress: 1 },
     name: "Battalion",
     rating: 3,
     num: -4208
-  }
+}
